d837bc6e8099e8f40e13c183506d12b7428a1ca5
Dominique Skaf <doskaf@yahoo.com>
2019-09-11 16:29:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,7 +9,8 @@ def countdown(integer)
 end
 
 def countdown_with_sleep(integer)
-  while integer >= 0 do
+  while integer >= 0
+    puts "#{integer} SECOND(S)!"
     sleep(2)
     integer -= 1
   end
